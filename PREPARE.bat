a0e1991a36aef411a6e253866b4d3a84df8e4fed
Fallensn0w <thimcederlund@hotmail.com>
2010-08-31 15:14:07 +0200

    Purpose: Used for preparing for making a SFX Archive.

--- PREPARE.bat
+++ PREPARE.bat
@@ -6,7 +6,7 @@ mkdir BUILD
 copy readline5.dll %CD%\BUILD
 copy irecovery.exe %CD%\BUILD
 copy %AppName%.exe %CD%\BUILD
-copy README %CD%\BUILD
+:copy README %CD%\BUILD
 copy TODO %CD%\BUILD
 
 echo Files are ready to be archived into SFX.
